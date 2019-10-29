598edfc86760b606453c10ea91581091f82ec689
MatteoMartinelliMM <matteo.martinelli@dsgroup.it>
2019-10-29 17:48:48 +0100

    Create view per 2 e 3 giocatori scopa cirulla briscola

--- lib/AggiungiGiocatori/AggiungiGiocatoriScopa.dart
+++ lib/AggiungiGiocatori/AggiungiGiocatoriScopa.dart
@@ -34,33 +34,34 @@ class AggiungiGiocatoriScopa extends StatefulWidget
 class AggiungiGIocatoriScopaState extends State<AggiungiGiocatoriScopa> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Align(
-              alignment: Alignment.topCenter,
-              child: DropdownButton<String>(
-                  value: widget.howmanyPlayer,
-                  isExpanded: true,
-                  items: widget.nrOfPlayer.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        child: Text(value),
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      widget.howmanyPlayer = value;
-                    });
-                  })),
-        ),
-        getBody()
-      ],
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+            child: Align(
+                alignment: Alignment.topCenter,
+                child: DropdownButton<String>(
+                    value: widget.howmanyPlayer,
+                    isExpanded: true,
+                    items: widget.nrOfPlayer.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text(value),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        widget.howmanyPlayer = value;
+                      });
+                    })),
+          ),
+          Expanded(child: Align(alignment: Alignment.center, child: getBody()))
+        ],
+      ),
     );
   }
 
@@ -78,12 +79,239 @@ class AggiungiGIocatoriScopaState extends State<AggiungiGiocatoriScopa> {
   Widget getBody() {
     switch (widget.howmanyPlayer) {
       case DUO:
-        break;
+        return duo();
       case TLE:
-        break;
+        return tle();
       case QUATLO:
         break;
     }
-    return null;
+    return tle();
+  }
+
+  Widget tle() {
+    return Align(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  flex: 3,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Container(
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      fit: BoxFit.fill,
+                                      image: AssetImage(
+                                          IMAGE_PATH + 'defuser.png'))),
+                            ),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
+                              border: OutlineInputBorder(),
+                              labelText: "Aggiungi o crea un nuovo giocatore",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Flexible(
+                  flex: 3,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Container(
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      fit: BoxFit.fill,
+                                      image: AssetImage(
+                                          IMAGE_PATH + 'defuser.png'))),
+                            ),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
+                              border: OutlineInputBorder(),
+                              labelText: "Aggiungi o crea un nuovo giocatore",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Center(
+            child: Image.asset(IMAGE_PATH + 'vs_small.png'),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Spacer(flex: 2,),
+              Flexible(
+                flex: 3,
+                child: Center(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Container(
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      fit: BoxFit.fill,
+                                      image: AssetImage(
+                                          IMAGE_PATH + 'defuser.png'))),
+                            ),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
+                              border: OutlineInputBorder(),
+                              labelText: "Aggiungi o crea un nuovo giocatore",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(flex: 2,),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget duo() {
+    return Align(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+            child: Card(
+              elevation: 2.0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Container(
+                            width: 80,
+                            height: 80,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: AssetImage(
+                                        IMAGE_PATH + 'defuser.png'))),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
+                              border: OutlineInputBorder(),
+                              labelText: "Aggiungi o crea un nuovo giocatore",
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Image.asset(IMAGE_PATH + 'vs_small.png'),
+          Padding(
+            padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+            child: Card(
+              elevation: 2.0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Container(
+                            width: 80,
+                            height: 80,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: AssetImage(
+                                        IMAGE_PATH + 'defuser.png'))),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
+                              border: OutlineInputBorder(),
+                              labelText: "Aggiungi o crea un nuovo giocatore",
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
